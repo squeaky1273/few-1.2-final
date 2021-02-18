365dea828e9007846b0b063ac58c597fee721b8b
squeaky1273 <pjriddell22@gmail.com>
2021-02-18 01:10:02 -0800

    basic title

---
+++ src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="Title">
+            <header>
+                <h1>Gen 1 Pokédex</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Title
