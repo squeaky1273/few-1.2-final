1cdf11ac959fe7bf679c6ebd95249e4351533fc5
squeaky1273 <pjriddell22@gmail.com>
2021-02-18 16:20:02 -0800

    started on indivdual pokemon info page

---
+++ src/components/Pokemon/Pokemon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Pokemon(props) {
+    const { name, number, image, type, weakness } = props
+    return (
+        <div className="pokemon">
+            <p>{ name }</p>
+            <p>{ number }</p>
+            <p>{ image }</p>
+            {/* <img src={`${process.env.PUBLIC_URL}images/${image[0]}`} /> */}
+            <p>{ type }</p>
+            <p>{ weakness }</p>
+        </div>
+    )
+}
+export default Pokemon;
