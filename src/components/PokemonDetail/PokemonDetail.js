3edb5346a815e8a51b524a6b7bb987307bdd4f4d
squeaky1273 <pjriddell22@gmail.com>
2021-03-01 02:14:14 -0800

    css change

--- src/components/PokemonDetail/PokemonDetail.js
+++ src/components/PokemonDetail/PokemonDetail.js
@@ -8,14 +8,16 @@ function Pokemon(props) {
     return (
         <div className="PokemonDetail">
             <h1>{ name }  #{ number }</h1>
-            <div className="PokemonDetail-image">
-                {/* Images credited to official Pokemon website */}
-                <img src={`${process.env.PUBLIC_URL}images/${image}`} width="320" height="320"/>
+            <div className="PokemonDetail-column">
+                <div className="PokemonDetail-image">
+                    {/* Images credited to official Pokemon website */}
+                    <img src={`${process.env.PUBLIC_URL}images/${image}`} width="500" height="500"/>
+                </div>
+                <h2>Type:</h2>
+                <p>{ type }</p>
+                <h2>Weaknesses:</h2>
+                <p>{ weakness }</p>
             </div>
-            <h2>Type:</h2>
-            <p>{ type }</p>
-            <h2>Weaknesses:</h2>
-            <p>{ weakness }</p>
         </div>
     )
 }
--- src/components/PokemonSpace/PokemonSpace.js
+++ src/components/PokemonSpace/PokemonSpace.js
@@ -9,7 +9,7 @@ function PokemonSpace(props) {
     <div className="PokemonSpace">
       <Link to={`/details/${id}`}>
         {/* Images credited to official Pokemon website */}
-        <img src={`${process.env.PUBLIC_URL}images/${image}`} width="316" height="316" alt="Hello" />
+        <img src={`${process.env.PUBLIC_URL}images/${image}`} />
       </Link>
       <Link to={`/details/${id}`}>
         <p># {number}</p>
